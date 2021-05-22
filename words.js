be3dffc2794765af9b829ba6bdd62519f8fdb3e0
MattPelyhes <mattpelyhes@gmail.com>
2021-05-22 12:44:10 -0500

    add word map function

--- words.js
+++ words.js
@@ -4,19 +4,21 @@ const removePunctuation = (str) => {
   return str.replace(regex, "");
 }
 
+//Function takes in a string and returns an obj conatining the unique words, word lengths, and number of occurences
 const wordMap = (str) => {
   //An object that will hold every unique word from a given string, with the word length and number of occurences
-  let wordObj = {};
+  const wordObj = {};
   //Remove all punctuation from a string, turn all the characeters lowecase, and then turn the string into an array of strings
   const cleanStr  = removePunctuation(str).toLowerCase().split(" ");
-
+  // Loop through the string array and add each unique word to the word obj. For duplicated words we add to the count
   for(let word of cleanStr){
-    
+    if(word.length && !wordObj[word]){
+      wordObj[word] = { "length" : word.length,"count" : 1 };
+    } else if(word.length && wordObj[word]){
+      wordObj[word]["count"]++;
+    }
   }
-  return cleanStr;
-
-
-
+  return wordObj;
 }
 
-console.log(wordMap("Hello, my name is Mr. Pelyhes, and I don't am ! Here to Talk*&%$#)$_%#@{}. ?/ What ~ do you think of That?"));
+console.log(wordMap("Hello, my name is is is is  Mr. Pelyhes, and I don't am and my here to to name hello ! Here to Talk*&%$#)$_%#@{}. ?/ What ~ do you think of That?"));
